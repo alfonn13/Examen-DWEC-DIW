047c5a52c0baf5675055e1811bde04cd7ce3b4ab
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2024-01-17 10:52:03 +0100

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,9 @@ const validar = evento => {
     //Nombre
     nombre.value.trim().length === 0 && mensajesErrores.push('El campo nombre no puede estar vacío')
 
-    //!/^[A-Z]*$/.test(nombre.value.trim()) && mensajesErrores.push('El nombre no empieza por mayuscula')
+    
+
+    !/^[A-Z]*$/.test(nombre.value.trim()) && mensajesErrores.push('El nombre no empieza por mayuscula')
 
     //Correo
     !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(correo.value.trim()) && mensajesErrores.push('Introduce una dirección de correo electrónico válida')
@@ -41,9 +43,9 @@ const validar = evento => {
 
 info.addEventListener("click", (e)=>{
     e.preventDefault();
-
+    radio.checked && alert("Hola")
+    radio1.checked && alert("Adios")
 })
-
 
 formulario.addEventListener("submit", validar)
 
